9ee44c923c10119f13699b13258380e37377df5b
sathish16787 <sathish16787@gmail.com>
2022-05-18 02:42:00 +0530

    reduced sort operation step

--- second_lowest_grade.py
+++ second_lowest_grade.py
@@ -8,36 +8,28 @@ if __name__ == '__main__':
         m.append(score)
         records.append(m)
 
-# print()   
-# print(records)
-# print()
 
 ##loop the nested list and fetch the index[1] value from all the nexted list
 mark=[]
 for i in range(len(records)):
     mark.append(records[i][1])  
-# print(mark)
-# print()
+
 
 #sort the list
 mark.sort()
-s_mark=mark
-s_mark.reverse()
-sorted_mark=s_mark
-# print(sorted_mark)
+mark.reverse()
+
 
 ##getting the minimum value from the list
-minimum=min(sorted_mark)
-# print(minimum)
-# print()
+minimum=min(mark)
+
 
 #finding the second minimum from the list
 old_i=0.0
-for i in range(len(sorted_mark)):
-    if minimum < sorted_mark[i] and sorted_mark[i] != minimum:
-        old_i=sorted_mark[i]       
-# print(old_i)
-# print()
+for i in range(len(mark)):
+    if minimum < mark[i] and mark[i] != minimum:
+        old_i=mark[i]       
+
 
 ##get the name of the second maximum 
 final_output=[]
