94a0c4713e2f752b6ca699ac7d6be4127be10979
sathish16787 <sathish16787@gmail.com>
2022-05-17 19:38:18 +0530

    removed sort from the program

--- find_runner_up.py
+++ find_runner_up.py
@@ -2,10 +2,12 @@ if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
     
-m=list(arr)
-m.sort()
-l=m
-maximum=max(m)
+# m=list(arr)
+# m.sort()
+# l=m
+
+l=list(arr)
+maximum=max(l)
 
 old_i=0
 for i in range(len(l)):
